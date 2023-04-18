cf1ca1d0bc7135228207da737f7f74d029b7b1ac
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-04-18 10:09:58 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (60.87%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,16 +3,16 @@ public:
     string mergeAlternately(string word1, string word2) {
         int i=0,j=0;
         string newStr="";
-        while(i<word1.size() and j<word2.size()){
-            newStr.push_back(word1[i++]);
-            newStr.push_back(word2[j++]);
+        while(i<word1.size() or j<word2.size()){
+            if(i<word1.size())newStr.push_back(word1[i++]);
+            if(j<word2.size())newStr.push_back(word2[j++]);
         }
-        if(i<word1.size()){
-            newStr+=word1.substr(i);
-        }
-        if(j<word2.size()){
-            newStr+=word2.substr(j);
-        }
+        // if(i<word1.size()){
+        //     newStr+=word1.substr(i);
+        // }
+        // if(j<word2.size()){
+        //     newStr+=word2.substr(j);
+        // }
         return newStr;
     }
 };
