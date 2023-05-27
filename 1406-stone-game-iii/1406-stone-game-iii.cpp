6477a5181b14e514566cd0460158f2a2e5bf4c8c
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-05-27 12:08:23 +0530

    Time: 288 ms (40.28%), Space: 132.2 MB (81.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        int notComputed = 1000000000;
-        vector<int> dp(n + 1, notComputed);
+        vector<int> dp(n + 1);
         
-        function<int(int)> f = [&](int i) -> int {
-            if (i == n) {
-                return 0;
-            }
-            if (dp[i] != notComputed) {
-                return dp[i];
-            }
-            dp[i] = stoneValue[i] - f(i + 1);
+        for(int i = n-1; i >= 0 ; i--) {
+            
+            dp[i] = stoneValue[i] - dp[i + 1];
             if (i + 2 <= n) {
-                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - f(i + 2));
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
             }
             if (i + 3 <= n) {
                 dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1]
-                    + stoneValue[i + 2] - f(i + 3));
+                    + stoneValue[i + 2] - dp[i + 3]);
             }
-            return dp[i];
-        };
+        }
 
-        int dif = f(0);
+        int dif = dp[0];
         if (dif > 0) {
             return "Alice";
         }
