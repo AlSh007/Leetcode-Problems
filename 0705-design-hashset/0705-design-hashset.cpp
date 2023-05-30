8b92ec23f9f7553949f0c940a2bf32d101c0f6e6
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-05-30 10:13:18 +0530

    Time: 108 ms (52.43%), Space: 50.2 MB (29.45%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,18 +1,35 @@
 class MyHashSet {
+    private:
+    int prime;
+    vector<list<int>> table;
+    
+    int hash(int key){
+        return key%prime;
+    }
+    
+    list<int>::iterator search(int key){
+        int h = hash(key);
+        return find(table[h].begin(),table[h].end(),key);
+    }
 public:
-    vector<bool> table;
-    MyHashSet() : table(1e6+1,false) {}
+    MyHashSet() : prime(10007), table(prime) {}
     
     void add(int key) {
-        table[key] = true;
+        int h = hash(key);
+        if(!contains(key))
+            table[h].push_back(key);
     }
     
     void remove(int key) {
-        table[key] = false;
+        int h = hash(key);
+        auto it = search(key);
+        if(it != table[h].end())
+            table[h].erase(it);
     }
     
     bool contains(int key) {
-        return table[key];
+        int h = hash(key);
+        return search(key) != table[h].end();
     }
 };
 
