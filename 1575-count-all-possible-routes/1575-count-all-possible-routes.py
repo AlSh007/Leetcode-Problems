9df80dd7766450ba756618d5814aedb08e15f24f
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-06-25 12:08:32 +0530

    Time: 7912 ms (5.26%), Space: 17.1 MB (91.73%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,20 +1,16 @@
 class Solution:
-    MOD = 1000000007
-    def helper(self,locations,city,finish,remainFuel,memo):
-        if remainFuel < 0:
-            return 0
-        if memo[city][remainFuel] is not None:
-            return memo[city][remainFuel]
-        
-        total = 1 if city==finish else 0
-        
-        for nextCity in range(len(locations)):
-            if city != nextCity and remainFuel >= abs(locations[city] - locations[nextCity]):
-                total = (total + self.helper(locations,nextCity,finish,remainFuel - abs(locations[city] - locations[nextCity]),memo))%self.MOD
-        memo[city][remainFuel] = total
-        return total
     def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        MOD = 1000000007
         n = len(locations)
-        memo = [[None]*(fuel+1) for i in range(n)]
-        return self.helper(locations,start,finish,fuel,memo)
         
+        dp = [[0] * (fuel + 1) for _ in range(n)]
+        for f in range(fuel + 1):
+            dp[finish][f] = 1
+        
+        for f in range(fuel + 1):
+            for city in range(n):
+                for nextCity in range(n):
+                    if nextCity != city and f >= abs(locations[nextCity] - locations[city]):
+                        dp[city][f] = (dp[city][f] + dp[nextCity][f - abs(locations[nextCity] - locations[city])]) % MOD
+        
+        return dp[start][fuel]  
