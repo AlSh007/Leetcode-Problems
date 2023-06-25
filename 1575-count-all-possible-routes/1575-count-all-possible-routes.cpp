0b16dde6668da0281f8632213ea4a4f87c042bf7
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-06-25 11:52:53 +0530

    Time: 1006 ms (5.53%), Space: 11.8 MB (42.54%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
-    int helper(vector<int>& locations,int city, int finish,int remainFuel,vector<vector<int>>& memo){
-        if(remainFuel < 0)
-            return 0;
-        if(memo[city][remainFuel] != -1)
-            return memo[city][remainFuel];
-        int total = city==finish ? 1 : 0;
-        for(int nextCity=0;nextCity<locations.size();nextCity++){
-            if (nextCity != city && remainFuel >= abs(locations[nextCity] - locations[city])) {
-                total = (total + helper(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), memo)) % MOD;
-            }
-        }
-        return memo[city][remainFuel] = total;
-    }
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-       int n = locations.size();
-        vector<vector<int>> memo(n,vector<int>(fuel + 1,-1));
-        return helper(locations,start,finish,fuel,memo);
-    }
+       const int MOD = 1000000007;
+        int n = locations.size();    
+        
+        vector<vector<int>> dp(n, vector<int>(fuel + 1, 0));
+        for (int f = 0; f <= fuel; f++) {
+            dp[finish][f] = 1;
+        }
+        
+        // for each fuel, we calculate all cross between cities
+        for (int f = 0; f <= fuel; f++) {
+            for (int city = 0; city < n; city++) {            
+                for (int nextCity = 0; nextCity < n; nextCity++) {               
+                    if (nextCity != city && f >= abs(locations[nextCity] - locations[city])) {
+                        dp[city][f] = (dp[city][f] + dp[nextCity][f - abs(locations[nextCity] - locations[city])]) % MOD;
+                    }
+                }
+            }            
+        }
+        return dp[start][fuel];    }
 };
