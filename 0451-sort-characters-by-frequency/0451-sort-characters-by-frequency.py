d1f361a2251cb56645909358568891f22adfbbea
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-08-26 12:22:21 +0530

    Time: 53 ms (69.70%), Space: 17.5 MB (89.81%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,7 +2,10 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         from collections import Counter
         cnt = Counter(s)
-        s = list(s)
-        s.sort(key = lambda x:(-cnt[x],x))
-        return ''.join(s)
+        arr = [[freq,c] for c, freq in cnt.items()]
+        arr.sort(key = lambda x:-x[0])
+        ans = []
+        for freq, char in arr:
+            ans.append(freq*char)
+        return ''.join(ans)
         
