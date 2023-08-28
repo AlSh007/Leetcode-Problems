16d5d3ec88aeb909a917cc3d6abdfbf48fe3ff87
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-08-28 20:54:06 +0530

    Time: 103 ms (66.76%), Space: 21 MB (66.88%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+        arr = sorted(dic, key = dic.get, reverse = True)
+        return arr[:k]
