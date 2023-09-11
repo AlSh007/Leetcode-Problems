70ed970bbfaf581dd3af7d19b12cdb3c8b22efb3
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-09-11 15:41:30 +0530

    Time: 130 ms (80.65%), Space: 17.3 MB (50.72%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -2,24 +2,11 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         m = len(matrix)
         n = len(matrix[0])
-        dp = [[float('inf')]*n for _ in range(m)]
-        def dfs(i, j):
-            if i==m-1 and j >= 0 and j < n:
-                return matrix[i][j]
-            if j >= n or j<0:
-                return float('inf')
-            if dp[i][j] != float('inf'):
-                return dp[i][j]
-            
-            down =  dfs(i+1,j)
-            lower_left =  dfs(i+1,j-1)
-            lower_right =  dfs(i+1,j+1)
-            ans = matrix[i][j] + min(down,lower_left,lower_right)
-            dp[i][j] = ans
-            return ans
-        
-        final_ans = float('inf')
-        for i in range(n):
-            final_ans = min(final_ans,dfs(0,i))
-        return final_ans
+        for i in range(1,m):
+            for j in range(n):
+                upper_left = matrix[i-1][j-1] if j-1 >= 0 else float('inf')
+                up = matrix[i-1][j]
+                upper_right = matrix[i-1][j+1] if j + 1 < n else float('inf')
+                matrix[i][j] += min(upper_left, up, upper_right)
+        return min(matrix[n-1])
         
