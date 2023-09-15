9b55a8077046a74bb65cf14f984f141f4ad6da93
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-09-15 12:23:57 +0530

    Time: 1716 ms (60.84%), Space: 131.8 MB (48.93%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        manhattan = lambda p1, p2 : abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        
+        n = len(points)
+        adj = defaultdict(list)
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = manhattan(points[i],points[j])
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
+                
+        cnt , ans, visited, heap = 1, 0, [0]*n, adj[0]
+        visited[0] = 1
+        heapq.heapify(heap)
+        
+        while heap:
+            dist, j = heapq.heappop(heap)
+            if not visited[j]:
+                visited[j], cnt, ans = 1, cnt + 1, ans + dist
+                for record in adj[j]:
+                    heapq.heappush(heap, record)
+                    
+                if cnt >= n:
+                    break
+        return ans
