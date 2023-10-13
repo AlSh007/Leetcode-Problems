8ba88883001e2348c97e0b0a4f55ce1af381f993
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-10-13 17:54:37 +0530

    Time: 542 ms (94.41%), Space: 23.5 MB (87.96%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        hold, free = -prices[0], 0
+        buy, sell = -prices[0], 0 #if we buy and sell on first day
         
         for i in range(1,n):
-            hold = max(hold,free-prices[i])
-            free = max(free,hold+prices[i]-fee)
+            buy = max(buy,sell-prices[i]) #profiit from buying will be last sell profit - today's stock price
+            sell = max(sell,buy+prices[i]-fee) #profit if we sell today
         
-        return free
+        return sell #gotta sell finally cant hold the stock
