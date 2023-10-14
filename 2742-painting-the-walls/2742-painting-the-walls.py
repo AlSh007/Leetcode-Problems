2e5af104864add4565e5f22d640894ba168e38fa
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-10-14 12:33:14 +0530

    Time: 1076 ms (62.22%), Space: 26.3 MB (46.67%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,17 +1,22 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         n = len(cost)
-        dp = [[-1]*(n+1) for _ in range(n+1)]
-        def solve(ind, walls):
-            if walls <= 0:
-                return 0
-            if ind < 0:
-                return inf
-            if dp[ind][walls] != -1:
-                return dp[ind][walls]
-            take = cost[ind] + solve(ind - 1, walls - time[ind] - 1)
-            not_take = 0 + solve(ind - 1, walls)
-            dp[ind][walls] = min(take, not_take)
-            return dp[ind][walls]
-        
-        return solve(n-1, n)
+        dp = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+
+        # Base case: If we have zero walls to paint, the cost is zero.
+        for i in range(n + 1):
+            dp[i][0] = 0
+
+        for ind in range(n):
+            for walls in range(1, n + 1):
+                take = cost[ind]  # Initialize take as the cost of the current wall
+                remaining_walls = walls - time[ind] - 1
+
+                if remaining_walls >= 0:
+                    take += dp[ind][remaining_walls]
+
+                not_take = dp[ind][walls]
+
+                dp[ind + 1][walls] = min(take, not_take)
+
+        return dp[n][n]
