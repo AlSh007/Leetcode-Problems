ef76c86b328614520833629d7ae567f790d28b3f
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-10-15 13:19:11 +0530

    Time: 147 ms (88.33%), Space: 16.1 MB (96.67%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -3,20 +3,26 @@ class Solution:
         MOD = 10**9 + 7
         arrLen = min(arrLen, steps //2 + 1)
         
-        dp = [[0]*(steps + 1) for _ in range(arrLen)]
+        dp = [0]*arrLen
         
-        dp[0][0] = 1
+        prevDp = [0]*arrLen
+        
+        prevDp[0] = 1
+        
         
         for step in range(1, steps + 1):
-            for index in range(arrLen - 1, -1, -1):
-                ans = dp[index][step - 1]
+            dp = [0]*arrLen
+            for index in range(arrLen - 1, -1, -1): #becuase our answer state is index = 0 that's why we are going in reverse
+                ans = prevDp[index]
                 
                 if index > 0:
-                    ans = (ans + dp[index - 1][step - 1]) %MOD
+                    ans = (ans + prevDp[index - 1]) %MOD
                 
                 if index < arrLen - 1:
-                    ans = (ans +  dp[index + 1][step - 1])%MOD
+                    ans = (ans +  prevDp[index + 1])%MOD
                     
-                dp[index][step] = ans
+                dp[index] = ans
+            
+            prevDp = dp
         
-        return dp[0][steps]
+        return dp[0]
