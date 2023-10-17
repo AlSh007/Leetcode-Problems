f23ff66f1de1ff9f221aa407e564eb4bdb528a19
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-10-17 11:51:27 +0530

    Time: 270 ms (62.87%), Space: 18 MB (91.62%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,30 +1,36 @@
-class Solution:
-    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
-        def find_root():
-            children = set(leftChild) | set(rightChild)
-            
-            for i in range(n):
-                if i not in children:
-                    return i
-            
-            return -1
+class UnionFind:
+    def __init__(self, n):
+        self.components = n
+        self.parents = list(range(n))
         
-        root = find_root()
-        if root == -1:
+    def union(self, parent, child):
+        parent_parent = self.find(parent)
+        child_parent = self.find(child)
+        
+        if child_parent != child or parent_parent == child_parent: #so it means that the child has more than one parent or parent whiich is supposed to be root also has a parent which is not allowed
             return False
         
-        seen = {root}
-        queue = deque([root])
+        self.components -= 1
+        self.parents[child_parent] = parent_parent
+        return True
         
-        while queue:
-            node = queue.popleft()
-            for child in [leftChild[node], rightChild[node]]:
-                if child != -1:
-                    if child in seen:
-                        return False
-                    
-                    queue.append(child)
-                    seen.add(child)
-                    
-        return len(seen) == n
+    def find(self, node):
+        if self.parents[node] != node:
+            self.parents[node] = self.find(self.parents[node])
+        
+        return self.parents[node]
+
+
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        uf = UnionFind(n)
+        
+        for node in range(n):
+            for child in [leftChild[node],rightChild[node]]:
+                if child == -1:
+                    continue
                 
+                if not uf.union(node, child):
+                    return False
+            
+        return uf.components == 1
