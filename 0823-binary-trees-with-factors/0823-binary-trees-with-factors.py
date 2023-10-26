8f52b2c93b90254a8a15be32fff157f451fc614d
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-10-26 11:46:16 +0530

    Time: 289 ms (80.42%), Space: 16.4 MB (87.08%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -8,8 +8,8 @@ class Solution:
             dp[num] = 1
         
         for i, num in enumerate(arr):
-            for j in range(i): #we are running for loop for count because if we use any number as a root more than one time it will be the same tree
-                if not(num%arr[j]) and num // arr[j] in dp:
+            for j in range(i): #we are running for loop until we reach our current number so we will check if our number has any factors, since its a sorted array factors will be on the left side of the number
+                if num%arr[j] == 0 and num // arr[j] in dp:
                     dp[num] += dp[arr[j]] * dp[num // arr[j]]
                     dp[num] %= MOD
         
