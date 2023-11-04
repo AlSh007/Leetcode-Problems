c0f4d8ba6f4428b40ff57cefb0f24bacb50ab1ba
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-11-04 12:31:18 +0530

    Time: 242 ms (98.33%), Space: 16.2 MB (98.69%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def minCut(self, s: str) -> int:
         n = len(s)
-        dp = [-1]*n
+        dp = [0]*(n+1)
+        dp[n] = 0
         if s == s[::-1]: return 0
         
         for i in range(1, len(s)):
             if s[:i] == s[:i][::-1] and s[i:] == s[i:][::-1]:
                 return 1
+        
         def isPalindrome(i , j):
             while i < j:
                 if s[i] != s[j]:
@@ -15,21 +17,18 @@ class Solution:
                 j -= 1
             
             return True
-                
-
-        def solve(i):
-            if i == n:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            
+        
+        for i in range(n-1, -1, -1):
             min_cost = float('inf')
             
-            for j in range(i,n):
+            for j in range(i, n):
                 if isPalindrome(i, j):
-                    cost  = 1 + solve(j + 1)
+                    cost = 1 + dp[j+1]
                     min_cost = min(min_cost, cost)
+            
             dp[i] = min_cost
-            return dp[i]
+                
+
         
-        return solve(0) - 1
+        
+        return dp[0] - 1
