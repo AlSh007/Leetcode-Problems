08728ce4c5ccd24bc5b954503e0e5ea545c03ded
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-11-05 13:34:41 +0530

    Time: 303 ms (98.81%), Space: 16.6 MB (84.23%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -2,11 +2,10 @@ class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
         cuts.extend([0,n])
         cuts.sort()
-        n = len(cuts)
         dp = [[0]*len(cuts) for _ in cuts]
         
         for i in range(len(cuts) - 1, -1, -1):
-            for j in range(i+2, n):
+            for j in range(i+2, len(cuts)):
                 dp[i][j] = cuts[j] - cuts[i] + min(dp[i][k] + dp[k][j] for k in range(i+1, j))
         
         return dp[0][-1]
