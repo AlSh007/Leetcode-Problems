49586efe4993352ab1e0591c6f0754af87d96318
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-11-10 13:27:53 +0530

    Time: 259 ms (18.91%), Space: 20.1 MB (41.34%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -3,19 +3,20 @@ class Solution:
         m = len(word1)
         n = len(word2)
         
-        dp = [[0]*(n+1) for _ in range(m + 1)]
+        dp = [[1000]*(n+1) for _ in range(m + 1)]
         
-        for i in range(n+1):
-            dp[0][i] = 0
+        def solve(i, j):
+            if i == m and j == n:
+                return 0
+            if i == m or j == n:
+                return max(m - i, n - j)
+            if dp[i][j] != 1000:
+                return dp[i][j]
+            
+            if word1[i] == word2[j]:
+                return solve(i + 1, j + 1)
+            dp[i][j] = 1 + min(solve(i+1, j), solve(i, j+1))
+            return dp[i][j]
+            
         
-        for i in range(m+1):
-            dp[i][0] = 0
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        
-        return (m + n) - 2*dp[m][n]
+        return solve(0,0)
