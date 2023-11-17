2d718f03560d081c68e3a65aa1cfa1eb786a916a
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-11-17 16:00:21 +0530

    Time: 220 ms (22.43%), Space: 17.1 MB (85.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -7,20 +7,22 @@ class Solution:
         def find_parent(a):
             if parent[a] != a:
                 parent[a] = find_parent(parent[a])
+            
             return parent[a]
         
         def union(a, b):
             pa = find_parent(a)
             pb = find_parent(b)
             
-            if pa == pb: return
-            pbig, psmall = (pa, pb) if size[pa]>size[pb] else (pb, pa)
+            if pa == pb:
+                return
+            pbig, psmall = (pa, pb) if size[pa] > size[pb] else (pb, pa)
+            size[pbig] += psmall
             parent[psmall] = pbig
-            size[pbig] += size[psmall]
         
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(i+1, n):
                 if isConnected[i][j]:
-                    union(i,j)
-                    
+                    union(i, j)
+        
         return len(set(find_parent(i) for i in range(n)))
