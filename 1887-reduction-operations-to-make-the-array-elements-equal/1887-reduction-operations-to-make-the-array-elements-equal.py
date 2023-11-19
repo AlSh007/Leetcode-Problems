e08e086126fa304e36409ddb2b65bc0710a68640
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-11-19 12:21:41 +0530

    Time: 838 ms (59.28%), Space: 23.9 MB (58.25%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -5,8 +5,8 @@ class Solution:
         increase = 0
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1]:
-                increase += 1
+                increase += 1 #everytime we get a larger number we will have to do more operations, since the larger elements will be decreased to next larger first we will increament this variable to 
             
-            ans += increase
+            ans += increase #we will add all the levels of increasing numbers to the final ans
         
         return ans
