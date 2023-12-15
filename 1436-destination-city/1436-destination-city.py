530c3583cd30823665a2ecbeba436084fc2c4630
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2023-12-15 11:17:11 +0530

    Time: 61 ms (61.48%), Space: 16.5 MB (13.03%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,11 +1,10 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        has_outgoing = set()
-        for i in range(len(paths)):
-            has_outgoing.add(paths[i][0])
+        journey = { f:t for f,t in paths}
         
-        for i in range(len(paths)):
-            if paths[i][1] not in has_outgoing:
-                return paths[i][1]
+        path = paths[0][0]
         
-        return ""
+        while path in journey:
+            path = journey[path]
+        
+        return path
