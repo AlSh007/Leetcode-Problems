d21a009dd10a1ea54385a2236e7326d943a9de12
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2024-02-02 11:13:23 +0530

    Time: 34 ms (71.12%), Space: 16.7 MB (53.11%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        ans = []
+        queue = deque(range(1,10))
+        while queue:
+            elem = queue.popleft()
+            if low <= elem <= high:
+                ans.append(elem)
+            last = elem % 10
+            if last < 9:
+                queue.append(elem * 10 + last + 1)
+        
+        return ans
