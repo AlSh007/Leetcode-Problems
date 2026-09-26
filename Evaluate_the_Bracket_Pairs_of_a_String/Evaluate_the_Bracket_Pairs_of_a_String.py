5f8165af194ff371b827dce5d019bc812d283213
Alok Sharma <85677626+AlSh007@users.noreply.github.com>
2026-09-26 12:28:48 +0530

    Add/update solution Evaluate_the_Bracket_Pairs_of_a_String.py

---
+++ Evaluate_the_Bracket_Pairs_of_a_String/Evaluate_the_Bracket_Pairs_of_a_String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        d = dict(knowledge)
+        res, i = [], 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = s.find(')', i + 1)
+                res.append(d.get(s[i+1:j], '?'))
+                i = j
+            
+            else:
+                res.append(s[i])
+            i += 1
+        
+        return "".join(res)
